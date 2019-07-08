c29734831520bfd14f9f72a19e6e4f293575a71d
Existanza <kraft.sc2@gmail.com>
2019-07-08 15:54:10 +0200

    Add files via upload
    Gutenberg scraper

---
+++ main.py
@@ -0,0 +1,22 @@
+import urllib.request
+import urllib.error
+from shutil import copyfile
+
+for n in range(50, 100000):
+    gutenberg_url = "http://www.gutenberg.org/cache/epub/" + str(n) + "/pg" + str(n) + ".txt"
+
+    print(gutenberg_url)
+
+    tmp_name = "src/temp.txt"
+    file_name = "src/" + str(n) + ".txt"
+
+    try:
+        urllib.request.urlretrieve(gutenberg_url, tmp_name)
+        print("Downloading " + file_name)
+
+        with open(tmp_name, encoding="latin-1") as datafile:
+            if 'Language: English' in datafile.read():
+                copyfile(tmp_name, file_name)
+                print("Successfully saved " + file_name)
+    except urllib.error.HTTPError as err:
+        print(err.code)
